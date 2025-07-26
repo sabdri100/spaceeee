d090ad34d408c694260207863e3e1c8b37297ea5
mariana-marrero4 <mariana.marrero4@upr.edu>
2025-07-26 00:47:38 -0400

    fixed start menu

--- menu.gd
+++ menu.gd
@@ -1,10 +1,9 @@
 extends Control
-
+  
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -12,6 +11,7 @@ func _process(delta: float) -> void:
 
 
 func _on_start_pressed() :
+	self.visible = false  
 	get_tree().change_scene_to_file("res://main.tscn")
 
 
