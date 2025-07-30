581403d936e0872c5b8033f977dc015960c1553a
nataliavera6 <natalia.vera6@upr.edu>
2025-07-30 19:18:22 -0400

    added safety net

--- Player/ship.gd
+++ Player/ship.gd
@@ -45,7 +45,7 @@ func _go_to_lose_screen():
 		get_tree().call_deferred("change_scene_to_file","res://lose_screen.tscn")
 
 func _on_ship_hit_box_body_entered(body: Node2D) -> void:
-	if GameState.current_level_scene_path != "":
+	if GameState.current_level_scene_path != "" && GameState.current_level_scene_path != null:
 		if GameState.current_level_scene_path == "res://Levels_logic/Level_1.tscn":
 			get_tree().change_scene_to_file("res://lose_screens/lose_screen.tscn")
 		if GameState.current_level_scene_path == "res://Levels_logic/Level_2.tscn":
