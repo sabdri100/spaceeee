11a57a0919a6f64121663a5fca41469a5098f472
sab <sabdrielyamir@gmail.com>
2025-07-31 02:34:47 -0400

    fix win screen restard and lose screen

--- win_screen.gd
+++ win_screen.gd
@@ -6,7 +6,7 @@ extends Control
 @onready var exit_button = $ExitButton
 
 func _on_continue_buton_pressed():
-	get_tree().change_scene_to_file("res://Level_1.tscn")
+	get_tree().change_scene_to_file("res://Levels_logic/Level_1.tscn")
 
 
 func _on_exit_button_pressed() -> void:
