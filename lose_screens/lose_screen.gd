50f0d11c07a8441bf281bd442420e7c6e76a9c95
Adriana Isabel <adriana.carrasquillo1@upr.edu>
2025-07-30 23:55:46 -0400

    Update lose_screen.gd

--- lose_screens/lose_screen.gd
+++ lose_screens/lose_screen.gd
@@ -2,10 +2,10 @@ extends Control
 
 @onready var Title = $Title
 @onready var inst = $Instrucctions
-@onready var continue_buton = $ContinueButon
+@onready var continue_button = $ContinueButon
 @onready var exit_button = $ExitButton
 
-func _on_continue_buton_pressed():
+func _on_continue_button_pressed():
 	if GameState.current_level_scene_path != "":
 		get_tree().change_scene_to_file(GameState.current_level_scene_path)
 	else:
